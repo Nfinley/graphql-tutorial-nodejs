3252866f2b16724cd31c40b1d8d016832b9d5abd
Nigel Finley <nfinleymusic@gmail.com>
2019-03-31 22:22:44 -0700

    Adding prisma client

--- src/index.js
+++ src/index.js
@@ -1,50 +1,56 @@
 // https://www.howtographql.com/graphql-js/2-a-simple-query/
-
+const { prisma } = require("./generated/prisma-client");
 const { GraphQLServer } = require("graphql-yoga");
 
-// 1
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-type Link {
-    id: ID!
-    description: String!
-    url: String!
-}
-`;
-
-// 2
-
-// Dummy Data
-const links = [
-  {
-    id: "link-0",
-    url: "www.howtographql.com",
-    description: "Fullstack tutorial for GraphQL"
-  },
-  {
-    id: "link-1",
-    url: "www.google.com",
-    description: "Google Website"
-  }
-];
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links
+    feed: (root, args, context, info) => {
+      return context.prisma.links();
+    },
+    link: (parent, { id }) => {
+      return links.filter(item => item.id === id)[0];
+    }
   },
-  Link: {
-    id: parent => parent.id,
-    description: parent => parent.description,
-    url: parent => parent.url
+  //   Dont need this because the server will infer this basic structure
+  //   Link: {
+  //     id: parent => parent.id,
+  //     description: parent => parent.description,
+  //     url: parent => parent.url
+  //   },
+  Mutation: {
+    post: (root, args, context) => {
+      return context.prisma.createLink({
+        url: args.url,
+        description: args.description
+      });
+    },
+    updateLink: (parent, args) => {
+      links.map(item => {
+        if (item.id === args.id) {
+          item.description = args.description || item.description;
+          item.url = args.url || item.url;
+        }
+      })[0];
+      const updatedLink = links.filter(item => item.id === args.id)[0];
+      return updatedLink;
+    },
+    deleteLink: (parent, args) => {
+      function itemToDelete(element) {
+        return element.id === args.id;
+      }
+      let deletedLink = links.filter(item => item.id === args.id)[0];
+      let index = links.findIndex(itemToDelete);
+      links.splice(index);
+      return deletedLink;
+    }
   }
 };
 
 // 3
 const server = new GraphQLServer({
-  typeDefs,
-  resolvers
+  typeDefs: "./src/schema.graphql",
+  resolvers,
+  context: { prisma }
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
